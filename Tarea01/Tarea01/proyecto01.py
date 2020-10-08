34d0f4ab366158348a045743a0a4dcb5aee24789
armando122 <armandoramirezg@ciencias.unam.mx>
2020-10-08 14:23:40 -0500

    Método peticiones() corregido

--- Tarea01/Tarea01/proyecto01.py
+++ Tarea01/Tarea01/proyecto01.py
@@ -13,15 +13,17 @@ EL programa devuelve en la salida estandar la ciudad, la descripción del clima,
 la temperatura mínima, la temperatura máxima y la humedad.
 """
 
-llave_api = '56de92f593ea497ac17dd421c9fed113'
+#Llave para usar OpenWeatherMap
+llave_api = {YOUR_API_KEY}
+#URL raiz para hacer peticiones
 url_base = 'http://api.openweathermap.org/data/2.5/weather?'
 
 def main():
     try:
         diccionario_ciudades = lectura('resources/dataset1.csv')  #Lectura de csv1
-        diccionario_ciudades_uno = lectura('resources/dataset2.csv') #Lectura de csv2
+        diccionario_ciudades.update(lectura('resources/dataset2.csv')) #Lectura de csv2
+
         climas = peticiones(diccionario_ciudades)
-        print(climas)
 
     except FileNotFoundError as e:
 
@@ -34,34 +36,39 @@ Recibe un diccionario con las ciudades de las cuales se quiere obtener el clima.
 Regresa un diccionario
 """
 def peticiones(diccionario):
+
     cont = 0
     nuevo_diccionario = { }
+
     for ciudad in diccionario.keys():
         if cont >= 30:
             waiter.wait([1] * 30)
             cont = 0
-        url = url_base + "q=" + ciudad + "&units=metric&appid=" + llave_api
+        url = url_base + "q=" + ciudad + "&units=metric&lang=es&appid=" + llave_api
         respuesta = requests.get(url)
-        if respuesta.status_code() != '404':
+
+        if respuesta.status_code != 404:
             nuevo_diccionario[ciudad] = respuesta.json()
             cont+= 1
             print('Success!')
-        else:
+
+        if not is_empty(diccionario[ciudad]):
             latitud = diccionario[ciudad][0]
             longitud = diccionario[ciudad][1]
-            url = url_base + "lat=" + latitud + "&lon=" + longitud + "&units=metric&appid=" + llave_api
+            url = url_base + "lat=" + latitud + "&lon=" + longitud + "&units=metric&lang=es&appid=" + llave_api
             respuesta = requests.get(url)
             nuevo_diccionario[ciudad] = respuesta.json()
             cont += 1
             print('Not Found.')
+
+        else:
+            continue
+
     return nuevo_diccionario
 
     #Por hacer:
-    #Peticiones a OpenWeatherMap, se proponen 30 cada 30 segundos. Utilizar waiters
-    #Si hay error con clave en vuelos internacionales se procede a intentar con
-    #Latitudes y longitudes si ningna opción funciona se regresa el error
     #Devolver datos del clima [Ciudad, descripción del clima, temp. mínima, temp. Máxima, Humedad]
-    #Pruebas. Al menos 3.
+    #Pruebas. Al menos faltan 2.
     #EXTRA: Procesar la información de manera simultanea
     #      y limitar peticiones concurrentes a web service para no saturarlo
 
@@ -71,7 +78,7 @@ Devulve un diccionario con la información del csv.
 """
 def lectura(hoja):
 
-    #Diccionaro para guardar las ciudades
+    #Diccionaro para guardar las ciudades (cache)
     diccionario = {}
 
     if hoja == 'resources/dataset1.csv':
@@ -98,6 +105,12 @@ def lectura(hoja):
     #Calusula en caso de que no se reciba el archivo correcto
     else:
         raise FileNotFoundError
+
+def is_empty(data_structure):
+    if data_structure:
+        return False
+    else:
+        return True
 
 if __name__ == '__main__':
     main()
