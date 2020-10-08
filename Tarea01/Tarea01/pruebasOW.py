5de7bb841faa8abaaa7790bf8ad21a51b44631e8
ceciliadelcarmenvillatororamos <cc.villatoror@ciencias.unam.mx>
2020-10-08 00:02:59 -0500

    Agrega acciones al método peticiones().
    Ahora el método empareja cada ciudad con su respectivo clima y si request no encuentra por el nombre de la ciudad, accede a su latitud y longitud para buscar el clima de nuevo.

--- Tarea01/Tarea01/pruebasOW.py
+++ Tarea01/Tarea01/pruebasOW.py
@@ -4,7 +4,7 @@ import csv
 import waiter
 
 llave_api = '56de92f593ea497ac17dd421c9fed113'
-url_base = 'api.openweathermap.org/data/2.5/weather?'
+url_base = 'http://api.openweathermap.org/data/2.5/weather?'
 
 
 def main():
@@ -14,14 +14,22 @@ def peticiones(diccionario):
     cont = 0
     nuevo_diccionario = { }
     for ciudad in diccionario.keys():
-        url = url_base + "q=" + ciudad + "&appid=" + llave_api
         if cont >= 30:
             waiter.wait([1] * 30)
             cont = 0
+        url = url_base + "q=" + "Guadalajara" + "&units=metric&appid=" + llave_api
         respuesta = requests.get(url)
-        if respuesta.status_code() == 200:
+        if respuesta.status_code() != 404:
+            nuevo_diccionario[ciudad] = respuesta.json()
+            cont+= 1
             print('Success!')
-        elif respuesta.status_code() == 404:
+        else:
+            latitud = diccionario[ciudad][0]
+            longitud = diccionario[ciudad][1]
+            url = url_base + "lat=" + latitud + "&lon=" + longitud + "&units=metric&appid=" + llave_api
+            respuesta = requests.get(url)
+            nuevo_diccionario[ciudad] = respuesta.json()
+            cont += 1
             print('Not Found.')
 
 
