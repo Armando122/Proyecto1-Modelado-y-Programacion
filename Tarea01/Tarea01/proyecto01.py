2dacc4d1607466d298e323bdc1ce99061fcb5d89
armando122 <armandoramirezg@ciencias.unam.mx>
2020-10-09 11:49:50 -0500

    Subiendo proyecto terminado faltas Readme

--- Tarea01/Tarea01/proyecto01.py
+++ Tarea01/Tarea01/proyecto01.py
@@ -25,6 +25,8 @@ def main():
 
         climas = peticiones(diccionario_ciudades)
 
+        impresion(climas)
+
     except FileNotFoundError as e:
 
         #Lo que se imprime en caso de que no se encuentre el archivo.
@@ -37,40 +39,49 @@ Regresa un diccionario
 """
 def peticiones(diccionario):
 
+    #Crea un contador para dosificar las peticiones
     cont = 0
+    #Crea un nuevo diccionario para emparejar las ciudades con su clima,
+    #este diccionario se regresará para impresión.
     nuevo_diccionario = { }
 
+    #Ciclo para recorrer todo el diccionario recibido.
     for ciudad in diccionario.keys():
         if cont >= 30:
+            #Despuñes de 30 peticiones, el programa espera 30 segundos.
             waiter.wait([1] * 30)
+            #Reinicia el contador.
             cont = 0
+        #Se realiza la petición a OpenWeather con el nombre de la ciudad y
+        #se recibe la respuesta.
         url = url_base + "q=" + ciudad + "&units=metric&lang=es&appid=" + llave_api
         respuesta = requests.get(url)
 
         if respuesta.status_code != 404:
+            #Si la petición es exitosa, se empareja el nombre de la ciudades
+            #con el clima obtenido en la petición.
             nuevo_diccionario[ciudad] = respuesta.json()
+            #Actualiza el contador al terminar la petición.
             cont+= 1
-            print('Success!')
 
-        if not is_empty(diccionario[ciudad]):
+        elif not is_empty(diccionario[ciudad]):
+            #Si la petición no tiene exito y la ciudad tiene cordenadas,
+            #se realiza una petición con ellas.
             latitud = diccionario[ciudad][0]
             longitud = diccionario[ciudad][1]
             url = url_base + "lat=" + latitud + "&lon=" + longitud + "&units=metric&lang=es&appid=" + llave_api
             respuesta = requests.get(url)
+            #Se empareja la ciudad con el clima recuperado.
             nuevo_diccionario[ciudad] = respuesta.json()
+            #Actualiza el contador al terminar la petición.
             cont += 1
-            print('Not Found.')
-
         else:
+            nuevo_diccionario[ciudad]  = respuesta.json()
+            cont += 1
             continue
-
+    #Regresa el dicionario que empareja todas las ciudades con sus climas.
     return nuevo_diccionario
 
-    #Por hacer:
-    #Devolver datos del clima [Ciudad, descripción del clima, temp. mínima, temp. Máxima, Humedad]
-    #Pruebas. Al menos faltan 2.
-    #EXTRA: Procesar la información de manera simultanea
-    #      y limitar peticiones concurrentes a web service para no saturarlo
 
 """
 Función para leer el csv.
@@ -106,11 +117,61 @@ def lectura(hoja):
     else:
         raise FileNotFoundError
 
+"""
+Función para determinar si una estructura de datos es vacía.
+Devuelve False si no es vacía, True en otro caso.
+"""
 def is_empty(data_structure):
     if data_structure:
         return False
     else:
         return True
+
+"""
+Función para imprimir la lista de ciudades con su clima.
+Imprime en la terminal el nombre de cada ciudad seguido de una ficha con la
+información del clima.
+"""
+def impresion(datos):
+    diccionario = datos
+    #Ciclo para recorrer todas las ciudades del diccionario dado.
+    for ciudad in diccionario:
+        #Accede al clima de cada ciudad y la guarda en una variable para
+        #acceder a ella después.
+        clima = diccionario[ciudad]
+
+        #Ficha de impresión para cada clima. Se incluyen sólo los datos
+        #más generales y se agregan las unidades.
+        if diccionario[ciudad]['cod'] != '400' and diccionario[ciudad]['cod'] != '404':
+            #Se hace casting a todas las variables con un tipo de dato diferente
+            #a String.
+            temperatura = str(clima['main']['temp'])
+            sens_ter = str(clima['main']['feels_like'])
+            temp_min = str(clima['main']['temp_min'])
+            temp_max = str(clima['main']['temp_max'])
+            presion = str(clima['main']['pressure'])
+            humedad = str(clima['main']['pressure'])
+
+            print( ciudad +
+            "\n Ciudad/Aeropuerto: " + clima['name'] + ", " + clima['sys']['country'] +
+            "\n Clima general: " + clima['weather'][0]['main'] +
+            "\n Descripción: " + clima['weather'][0]['description'] +
+            "\n Temperatura: " + temperatura + " ºC" +
+            "\n Sensación térmica: " + sens_ter + " ºC" +
+            "\n Temp. mínima: " + temp_min + " ºC" +
+            "\n Temp. máxima: " + temp_max + " ºC" +
+            "\n Presión: " + presion + " nPa" +
+            "\n Humedad: " + humedad + "%" +
+            "\n -------------------------------------"
+            )
+        elif diccionario[ciudad]['cod'] == '400':
+            print(ciudad, ': No se puede acceder a la información. \
+                  \n\tCoordenadas incorrectas. \
+                  \n -------------------------------------')
+        else:
+            print(ciudad, ': No se puede acceder a la información. \
+                  \n\tLas ciudades no deben estar en clave IATA. \
+                  \n -------------------------------------')
 
 if __name__ == '__main__':
     main()
--- Tarea01/Tarea01/tests.py
+++ Tarea01/Tarea01/tests.py
@@ -9,6 +9,8 @@ Pruebas para el proyecto01.
 
 class PruebasUnitarias(unittest.TestCase):
 
+    #test_cache que prueba la integridad de los diccionarios usados para
+    #el modelado del cache
     def test_cache(self):
         diccionario = {'TLC':['193.371', '-99.566']}
         diccionario_vac = {}
@@ -16,8 +18,25 @@ class PruebasUnitarias(unittest.TestCase):
         self.assertEqual(proyecto01.lectura('resources/dataset1.csv')['TLC'],diccionario['TLC'])
         self.assertRaises(FileNotFoundError, proyecto01.lectura,'hola.csv')
 
+    #test_is_empty que prubea la correctitud del método is_empty para
+    #estructuras de datos vacías.
+    def test_is_empty(self):
+        diccionario = {'llave':'clave'}
+        diccionario_vac = {}
+        self.assertEqual(proyecto01.is_empty(diccionario), False)
+        self.assertEqual(proyecto01.is_empty(diccionario_vac), True)
+
+    #test_peticiones que prueba que las peticiones a la API de OpenWeatherMap
+    #se hagan de manera correcta.
     def test_peticiones(self):
-        d
+        invalido = {'MXN': []}
+        invalido_uno = {'MXN': ['1a','3']}
+        error_m = {'MXN': {'cod': '404', 'message': 'city not found'}}
+        error_l = {'MXN': {'cod': '400', 'message': 'wrong latitude'}}
+
+        self.assertEqual(proyecto01.peticiones(invalido), error_m)
+        self.assertEqual(proyecto01.peticiones(invalido_uno),error_l)
+
 
 if __name__ == '__main__':
     unittest.main()
