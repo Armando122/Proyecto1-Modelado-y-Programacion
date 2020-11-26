e2a3cc647b47646e0ff3c4ba485c00d8214a49aa
armando122 <armandoramirezg@ciencias.unam.mx>
2020-11-25 23:39:05 -0600

    Subiendo código codumentado

--- Proyecto2/src/main/java/MyP/proyectoDos/Imagen.java
+++ Proyecto2/src/main/java/MyP/proyectoDos/Imagen.java
@@ -1,13 +1,54 @@
 package MyP.proyectoDos;
 
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+
 /**
  * Clase Imagen
  */
 public class Imagen {
-  //Atributos
+
+  /* Atributos de Imagen. */
+  /* Colores para colorear los píxeles. */
+  private final Color blanco = new Color(255,255,255);
+  private final Color negro = new Color(0,0,0);
+
+  /* BufferedImage, copia de la imagen. */
+  /* String, nombre de la imagen sin la extensión. */
+  /* Umbral para determinar si el píxel es de una nube o cielo. */
+  private final double umbral = 0.95;
+
 
   /**
-   * Constructor
+   * Constructor para imagen.
+   * @param BufferedImage imagen: imagen a analizar.
+   * @param String nombre nombe de la imagen.
    */
-  public Imagen() {}
+  public Imagen(BufferedImage imagen, String nombre) {}
+
+  /**
+   * Método calculaIndice.
+   * Que se encarga de calcular el índice de cobertura nubosa
+   * de la imagen.
+   */
+  public void calculaIndice() {
+    //Calcula el indice y colorea la imagen.
+  }
+
+  /**
+   * Método obtenerNombre. Devuelve el nombre de la imagen.
+   * @return String nombre de imagen.
+   */
+  public String obtenerNombre() {
+    return this.nombre;
+  }
+
+  /**
+   * Método obtenerImagenBN. Devuelve la imagen en blanco y negro.
+   * @return BufferedImage imagenBN imagen en blanco y negro.
+   */
+  public BufferedImage obtenerImagenBN() {
+    return this.imagen;
+  }
+
 }
--- Proyecto2/src/main/java/MyP/proyectoDos/Proyecto2.java
+++ Proyecto2/src/main/java/MyP/proyectoDos/Proyecto2.java
@@ -1,13 +1,42 @@
 package MyP.proyectoDos;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
 /**
- * Hello world!
+ * Proyecto2 que recibe una imagen en formato .jpeg y devuvelve el indice
+ * de cobertura nubosa en la salida estándar. Regresa también la imagen
+ * segmentada con las nubes en blanco y el cielo negro, si se recibe
+ * la etiqueta s.
  *
  */
-public class Proyecto2 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Proyecto2 {
+
+    /*
+     * Método para leer la imagen recibida.
+     * Devuelve una copia de la imagen (BufferedImage).
+     * Arroja IOException si no se encuentra la imagen.
+     */
+    private static BufferedImage leer(String nombre) throws IOException {}
+
+    /**
+     * Método para escribir la nueva imagen.
+     */
+    private static void guardarImagen() {}
+
+    public static void main( String[] args ) {
+
+      /*
+      1.try-catch de lectura de imagen.
+      2.imagen = Llamar al método leer
+      2.Construir un objeto de Imagen con imagen como parametro y nombre
+      de imagen.
+      3. Devovler el CCI.
+      4.Revisar si está la etiqueta S o s y guardar la imagen si es así.
+      5.Lanzar una excepción si la etiqueta es incorrecta.
+      */
+
     }
 }
