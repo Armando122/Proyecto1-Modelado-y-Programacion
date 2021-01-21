a0fae8233a24e7023e2d317bedf330aeaa036fce
Armando Ramírez <53309070+Armando122@users.noreply.github.com>
2021-01-21 13:49:27 -0600

    Subiendo main

--- esquema_shamir/src/main/java/com/MyP/proyecto/EsquemaShamir.java
+++ esquema_shamir/src/main/java/com/MyP/proyecto/EsquemaShamir.java
@@ -49,6 +49,170 @@ public class EsquemaShamir {
     if (args[0].equals("d") && args.length != 3) {
       uso();
     }
+
+    /* Opción de cifrar. */
+    if (args[0].equals("c")) {
+      /* Asignación de datos. */
+      String archivoPuntos = args[1];
+      String documentoClaro = args[4];
+      int nPuntos = 0;
+      int minPuntos = 0;
+
+      try {
+        nPuntos = Integer.parseInt(args[2]);
+        minPuntos = Integer.parseInt(args[3]);
+
+        if (nPuntos <= 2) {
+          System.err.println("Se requieren más de 2 puntos.");
+          System.exit(1);
+        }
+        if (minPuntos <= 1 || minPuntos > nPuntos) {
+          System.err.println("Los puntos mínimos no pueden ser menores que 1"
+                 + " ni mayores que los puntos totales.");
+          System.exit(1);
+        }
+
+      } catch(NumberFormatException eNum) {
+        System.err.println("El dato ingresado como parámetro no es un número.");
+        System.exit(1);
+      }
+
+      String nombCompletArch = "";
+      String soloNombreArch = "";
+      /* Quitamos la ruta del archivo. */
+      for (int i = 0; i<documentoClaro.length(); i++) {
+        char caracter = documentoClaro.charAt(i);
+        String signoMalo = "/";
+        if (caracter == signoMalo.charAt(0)) {
+          nombCompletArch = documentoClaro.substring(i+1);
+        }
+      }
+      /* Quitamos la extensión del documento. */
+      for (int j = 0; j<nombCompletArch.length(); j++) {
+        char caracter = nombCompletArch.charAt(j);
+        String signoMalo = ".";
+        if (caracter == signoMalo.charAt(0)) {
+          soloNombreArch = nombCompletArch.substring(0,j);
+        }
+      }
+
+      /* Obtener contraseña usuario. */
+      char[] contrasenaUsr = null;
+      Console entrada = null;
+      String contrasena = null;
+      try {
+        entrada = System.console();
+        if (entrada != null) {
+          contrasenaUsr = entrada.readPassword();
+        }
+        contrasena = String.valueOf(contrasenaUsr);
+      } catch(Exception e) {
+      }
+
+      /* Generamos contraseña segura. */
+      String contrasenaSegura = "";
+      try {
+        contrasenaSegura = Contrasena.generaContrasena(contrasena);
+      } catch(Exception e) {
+      }
+
+      Polinomio puntos = new Polinomio(contrasenaSegura, minPuntos, nPuntos);
+      ArrayList<BigInteger> parejas = puntos.evaluaHorner();
+      ArrayList<String> parejasOrdena = new ArrayList<String>();
+      /* Ordenamos los puntos. */
+      String pareja = "(";
+      for (BigInteger cordenada : parejas) {
+        if (parejas.indexOf(cordenada) % 2 == 0) {
+          pareja += cordenada.toString() + ", ";
+        } else {
+          pareja += cordenada.toString() + ")";
+          parejasOrdena.add(pareja);
+          pareja = "(";
+        }
+      }
+
+      /* Leemos el archivo. */
+      ArrayList<String> archivoOrig = new ArrayList<String>();
+      try {
+        archivoOrig = Archivo.leerArchivo(documentoClaro);
+      } catch(IOException e) {
+        System.err.println("Archivo" + e.getMessage() + "no encontrado o inexistente.");
+      }
+
+      /* Se encripta. */
+      ArrayList<String> archivoCifrado = new ArrayList<String>();
+      /*try {
+        archivoCifrado = Cifrado.cifra(contrasenaSegura, archivoOrig);
+      } catch(Exception e) {
+      }*/
+      archivoCifrado.add(0, nombCompletArch);
+
+      /* Guardamos el archivo cifrado y las n evaluaciones. */
+      try {
+        Archivo.guardaArchivo("encriptado.aes", archivoCifrado);
+        Archivo.guardaArchivo("encriptado.frg", parejasOrdena);
+      } catch(IOException e) {
+        e.getMessage();
+      }
+
+    }
+
+    /* Opción para descifrar. */
+    if (args[0].equals("d")) {
+
+      /* Obtención de datos. */
+      String evaluaciones = args[1];
+      String archivoCifrado = args[2];
+      ArrayList<String> puntos = new ArrayList<String>();
+      ArrayList<String> archivo = new ArrayList<String>();
+
+      try {
+        puntos = Archivo.leerArchivo(evaluaciones);
+        archivo = Archivo.leerArchivo(archivoCifrado);
+      } catch(IOException e) {
+        System.err.println("Archivo" + e.getMessage() + "no encontrado o inexistente.");
+      }
+
+      String nombreArch = archivo.remove(0);
+
+      ArrayList<BigInteger> parejasListas = new ArrayList<BigInteger>();
+      for (String pareja : puntos) {
+        String inicio = pareja.replace("(", "");
+        String fin = pareja.replace(")", "");
+        String[] parejas = fin.split(", ");
+        BigInteger abcisa = new BigInteger(parejas[0]);
+        BigInteger ordenada = new BigInteger(parejas[1]);
+        parejasListas.add(abcisa);
+        parejasListas.add(ordenada);
+      }
+
+      /* Obtenemos la llave. */
+      Interpolacion inicioLlave = new Interpolacion();
+      String llavecita = inicioLlave.reconstruye(parejasListas);
+
+      /* Desencriptamos el archivo. */
+      ArrayList<String> descifrado = new ArrayList<String>();
+      try {
+        for (String aDes : archivo) {
+          Descifrado descifrador = new Descifrado();
+          String descif = descifrador.descifra(llavecita, aDes);
+          descifrado.add(descif);
+        }
+      } catch(Exception e) {
+      }
+
+      /* Guardamos el archivo. */
+      try {
+        Archivo.guardaArchivo(nombreArch, descifrado);
+      } catch(IOException e) {
+        e.getMessage();
+      }
+
+    }
+
+    /* Si ninguna condición se cumple. */
+    uso();
+
   }
 
 }
