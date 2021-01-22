ce3a524823b5f9d1a6898454303b9a10000a4227
Armando Ramírez <53309070+Armando122@users.noreply.github.com>
2021-01-22 11:10:04 -0600

    Actualizando tests

--- esquema_shamir/src/test/java/com/MyP/proyecto/TestCifrado.java
+++ esquema_shamir/src/test/java/com/MyP/proyecto/TestCifrado.java
@@ -4,25 +4,32 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import com.MyP.proyecto.Cifrado;
+import java.util.ArrayList;
 
 /**
  * Pruebas unitarias para la clase Cifrado.
  */
 public class TestCifrado {
 
-  private String llave = "e3b0c44298fc1c149afbf4c8996fb924
-                          27ae41e4649b934ca495991b7852b855";
+  private String llave = "e3b0c44298fc1c149afbf4c8996fb924"
+                       + "27ae41e4649b934ca495991b7852b855";
   private String texto = "Este bloque será cifrado, hasta pronto. :)";
-  private String textoC = "Qh2vt8uVWpDVJ0FvF9I4SyaBhwDqLPQ6UyT4Pi2urDtFoKCaoUA2uqXhL9I4TFYM";
+  private String textoC = "FK9vGMtv/3Jx62/VsP/JG/Ue7wgy93L+HZG3Rfbrdw0n8pJ1P1FJ/8RdRcDlxzbY";
 
   /**
    * Prueba unitaria para el método cifra.
    */
   @Test public void TestCifra() {
-    String cifrado = Cifrar.cifra(llave, texto);
+    String cifrado = "";
+    Cifrado prueba = new Cifrado();
+    try {
+      cifrado = prueba.cifra(llave, texto);
+      Assert.assertTrue(cifrado != null);
+      Assert.assertTrue(cifrado.length() > 0);
+      Assert.assertTrue(cifrado.equals(textoC));
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
 
-    Assert.assertTrue(cifrado != null);
-    Assert.assertTrue(cifrado.length() > 0);
-    Assert.assertTrue(cifrado.equals(textoC));
   }
 }
