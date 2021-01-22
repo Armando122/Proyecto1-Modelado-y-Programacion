c883c962b0ea70fb70bc65edc139614ead064670
Armando Ramírez <53309070+Armando122@users.noreply.github.com>
2021-01-22 11:11:38 -0600

    Actualizando tests

--- esquema_shamir/src/test/java/com/MyP/proyecto/TestDescifrado.java
+++ esquema_shamir/src/test/java/com/MyP/proyecto/TestDescifrado.java
@@ -4,14 +4,15 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import com.MyP.proyecto.Descifrado;
+import java.util.ArrayList;
 
 /**
  * Pruebas unitarias para la clase Descifrado.
  */
 public class TestDescifrado {
 
-  private String llave = "e3b0c44298fc1c149afbf4c8996fb924
-                  27ae41e4649b934ca495991b7852b855";
+  private String llave = "e3b0c44298fc1c149afbf4c8996fb924"
+                       + "27ae41e4649b934ca495991b7852b855";
   private String textoD = "Este bloque será cifrado, hasta pronto. :)";
   private String texto = "Qh2vt8uVWpDVJ0FvF9I4SyaBhwDqLPQ6UyT4Pi2urDtFoKCaoUA2uqXhL9I4TFYM";
 
@@ -19,11 +20,17 @@ public class TestDescifrado {
    * Prueba unitaria para el método descifra.
    */
   @Test public void testDescifra() {
-    String descifrado = Descifrar.descifra(llave, texto);
+    String descifrado = null;
+    Descifrado descrip = new Descifrado();
+    try {
+      descifrado = descrip.descifra(llave, texto);
+      Assert.assertTrue(descifrado != null);
+      Assert.assertTrue(descifrado.length() > 0);
+      Assert.assertTrue(descifrado.equals(textoD));
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
 
-    Assert.assertTrue(descifrado != null);
-    Assert.assertTrue(descifrado.length() > 0);
-    Assert.assertTrue(descifrado.equals(textoD));
   }
 
 }
