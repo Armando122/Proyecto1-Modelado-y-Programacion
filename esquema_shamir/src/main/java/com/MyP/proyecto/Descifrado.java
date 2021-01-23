a1eec521eb0068e0c8b9e006eef46b04cbd420f0
ceciliadelcarmenvillatororamos <cc.villatoror@ciencias.unam.mx>
2021-01-22 18:14:23 -0600

    Arregla advertencias de la clase Descifrado.
    Elimina redundancias, clases importadas inutilizadas.

--- esquema_shamir/src/main/java/com/MyP/proyecto/Descifrado.java
+++ esquema_shamir/src/main/java/com/MyP/proyecto/Descifrado.java
@@ -1,7 +1,6 @@
 package com.MyP.proyecto;
 
-import java.lang.IllegalArgumentException;
-import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
@@ -18,65 +17,6 @@ public class Descifrado {
 
   /* Constructor privado para evitar instanciación. */
   public Descifrado() {}
-
-  /**
-   * Método que calcula cada Pi(x) usando el método de Lagrange.
-   * En el caso de este proyecto, x = 0.
-   * @param x La variable en la que estamos evaluando.
-   * @param ordenadas El arreglo de todas las ordenadas para
-   *                  calcular el resultado.
-   * @param modsz El módulo del campo en el que estamos.
-   * @return BigInteger[] El arreglo que en cada entrada i está
-   *                  el resultado obtenido de Pi(x).
-   */
-  public static BigInteger[] multiplicaciónLagrange(
-          BigInteger x, BigInteger[] ordenadas, BigInteger modsz){
-    BigInteger resultado = new BigInteger("1");
-    BigInteger[] pI = new BigInteger[ordenadas.length];
-
-    for(int i = 0; i<ordenadas.length; i++){
-      for (int j = 0; j < ordenadas.length; j++){
-        if(i != j){
-          BigInteger dividendo = x.add(ordenadas[i].negate()).mod(modsz);
-          BigInteger divisor = ordenadas[i].add(ordenadas[j].negate()).modInverse(modsz);
-
-          BigInteger división = dividendo.multiply(divisor).mod(modsz);
-
-          resultado.multiply(división);
-        }
-      }
-      pI[i] = resultado;
-      resultado = new BigInteger("1");
-    }
-    return pI;
-  }
-
-  /**
-   * Método que calcula el término independiente del polinomio
-   * de Lagrange. La longitud de ambos arreglos debe ser la misma.
-   * @param abscisas Arreglo con todos las evaluaciones de cada punto.
-   * @param pI Arreglo que contiene las Pi(x).
-   * @param modsz Módulo del campo en el que se está trabajando.
-   * @throws Exception si los arreglos dados no tienen la misma logitud.
-   * @return Regresa la suma del producto de cada abscisa con su
-   * correspondiente Pi(x). Si x = 0, regresa el valor de la llave
-   * de cifrado.
-   */
-  public static BigInteger polinomioLagrange(BigInteger[] abscisas,
-                                             BigInteger[] pI,
-                                             BigInteger modsz) throws Exception{
-    if(abscisas.length != pI.length){
-      throw new Exception("La longitud de los arreglos no coincide");
-    }
-
-    BigInteger resultado = new BigInteger("0");
-
-    for (int i = 0; i < pI.length; i++){
-      resultado.add(abscisas[i].multiply(pI[i]));
-    }
-    return resultado.mod(modsz);
-  }
-
 
   /**
    * Método descrifra que desencripta la línea de texto recibida.
@@ -97,11 +37,10 @@ public class Descifrado {
      Cipher modo = Cipher.getInstance("AES");
      SecretKeySpec secreto = new SecretKeySpec(datos, "AES");
      modo.init(Cipher.DECRYPT_MODE, secreto);
-     byte[] prim = Base64.getDecoder().decode(linea.getBytes("UTF-8"));
+     byte[] prim = Base64.getDecoder().decode(linea.getBytes(StandardCharsets.UTF_8));
      byte[] linBytes = modo.doFinal(prim);
-     String linDecript = new String(linBytes,"UTF-8");
 
-     return linDecript;
+     return new String(linBytes, StandardCharsets.UTF_8);
   }
 
 }
